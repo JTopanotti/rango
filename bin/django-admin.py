e88423647d61a35eaf7fb625dfba707ff9290ec8
Jonathan Topanotti <jontopanotti@gmail.com>
2017-07-08 18:33:34 -0300

    First commit

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jonathan/workspace/web/tango_with_django/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says her there partner!")
+    return HttpResponse("Rango says her there partner!<br/><a href='/rango/about/'>About</a>")
+
+def about(request):
+    return HttpResponse("Rangso says here is the about page.<br/>Back to <a href='/rango/'>index</a>")
