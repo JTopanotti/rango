c2b3dac39648954a5643b564bee2507a278e4bfe
Jonathan Topanotti <jontopanotti@gmail.com>
2017-07-10 15:13:50 -0300

    About page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says her there partner!<br/><a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+    #return HttpResponse("Rango says her there partner!<br/><a href='/rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("Rangso says here is the about page.<br/>Back to <a href='/rango/'>index</a>")
+    return render(request, 'rango/about.html')
+    #return HttpResponse("Rangso says here is the about page.<br/>Back to <a href='/rango/'>index</a>")
