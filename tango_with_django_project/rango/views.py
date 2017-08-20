4c802434418c6820df05c0aca95a6c895755a6e4
jonathan <jonathan@localhost.localdomain>
2017-08-20 18:12:02 -0300

    Slug

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
+
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context=context_dict)
-    #return HttpResponse("Rango says her there partner!<br/><a href='/rango/about/'>About</a>")
 
 def about(request):
     return render(request, 'rango/about.html')
-    #return HttpResponse("Rangso says here is the about page.<br/>Back to <a href='/rango/'>index</a>")
