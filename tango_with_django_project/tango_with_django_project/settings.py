6e6ea534d6e0b17a95d58841c453f2fd20b2e151
Jonathan Topanotti <jontopanotti@gmail.com>
2017-12-29 09:06:09 -0200

    Finished Chapter 11, Password change functionality not working through Django Registration Redux, future solo implementation.

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -139,3 +139,11 @@ REGISTRATION_OPEN = True
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
 LOGIN_REDIRECT_URL = '/rango/'
+
+#Email backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'jontopanotti@gmail.com'
+EMAIL_HOST_PASSWORD = 'elF@MjolnirgreyjoY'
